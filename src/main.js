722a1d193d080880b695d7672e845a265f95de20
Curran Gehring <curran@tuckermilling.com>
2025-06-30 11:05:20 -0500

    Replace Vite starter with working Framework7 logger app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Framework7 from 'framework7/lite-bundle';
+import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
+import App from './App.vue';
+import routes from './routes';
 
-createApp(App).mount('#app')
+Framework7.use(Framework7Vue);
+
+const app = createApp(App);
+registerComponents(app);
+
+// Pass routes to Framework7-Vue root component via props
+app.mount('#app');
