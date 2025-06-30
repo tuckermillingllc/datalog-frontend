3ce0b727fe069c501181a6a0e243d0acfe18ac1e
tuckermillingllc <102198923+tuckermillingllc@users.noreply.github.com>
2025-06-30 16:41:17 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import Framework7 from 'framework7/lite-bundle';
-import 'framework7/css/framework7.bundle.css';
 import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
 import 'node_modules/framework7/framework7-bundle.css';
 import App from './App.vue';
