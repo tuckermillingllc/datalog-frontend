3be17ca9ab00f15b7c7e9884973cefe041a941e6
Curran Gehring <curran@tuckermilling.com>
2025-07-02 15:52:17 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -45,11 +45,14 @@
         </f7-navbar>
 
         <!-- Home Page -->
-        <f7-block v-if="currentPage === 'home'">
-          <f7-block-title class="text-align-center">Welcome to DataLog System</f7-block-title>
-          <f7-block class="text-align-center">
-            <p>Select a logging system to get started</p>
-          </f7-block>
+        <!--
+<f7-block v-if="currentPage === 'home'">
+  <f7-block-title class="text-align-center">Welcome to DataLog System</f7-block-title>
+  <f7-block class="text-align-center">
+    <p>Select a logging system to get started</p>
+  </f7-block>
+-->
+
             
           <f7-block>
             <div class="nav-buttons">
