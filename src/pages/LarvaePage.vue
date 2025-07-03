bb86d3c3c1e1fd8798b9f73b35263a46f124eb2d
Curran Gehring <curran@tuckermilling.com>
2025-07-03 11:35:40 -0500

    Update LarvaePage.vue

--- src/pages/LarvaePage.vue
+++ src/pages/LarvaePage.vue
@@ -2,32 +2,43 @@
   <f7-page name="larvae">
     <f7-navbar title="Larvae Logs" back-link="Back">
       <f7-nav-right>
-        <f7-link panel-open="left" icon-ios="f7:bars" />
+        <f7-link panel-open="left" icon-ios="f7:bars" icon-md="material:menu" />
       </f7-nav-right>
     </f7-navbar>
 
-    <!-- Segmented Control for tabs -->
-     <f7-block>
+    <!-- Segmented Control for 3 tabs -->
+    <f7-block>
       <f7-segmented raised>
-        <f7-button :class="{ 'button-active': activeTab === 'form' }" @click="activeTab = 'form'">
+        <f7-button 
+          :class="{ 'button-active': activeTab === 'form' }"
+          @click="activeTab = 'form'"
+        >
           New Log
         </f7-button>
-        <f7-button :class="{ 'button-active': activeTab === 'list' }" @click="activeTab = 'list'">
+        <f7-button 
+          :class="{ 'button-active': activeTab === 'list' }"
+          @click="activeTab = 'list'"
+        >
           View Logs
         </f7-button>
-        <f7-button :class="{ 'button-active': activeTab === 'update' }" @click="activeTab = 'update'">
+        <f7-button 
+          :class="{ 'button-active': activeTab === 'update' }"
+          @click="activeTab = 'update'"
+        >
           Update Results
         </f7-button>
       </f7-segmented>
     </f7-block>
 
-     <!-- Tab Content -->
+    <!-- Tab Content -->
     <div v-show="activeTab === 'form'">
       <LarvaeLogForm />
     </div>
+    
     <div v-show="activeTab === 'list'">
       <LarvaeLogList />
     </div>
+
     <div v-show="activeTab === 'update'">
       <LarvaeUpdateResults />
     </div>
